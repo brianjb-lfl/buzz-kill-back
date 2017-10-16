d16b250ccb477155b7a02df5d6d10e2e588572e9
Brian Besaw <bjbesaw@outlook.com>
2017-10-16 12:41:37 -0400

    added basic endpoint, tested sat with test data through postman

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -9,31 +11,73 @@ const {dbConnect} = require('./db-mongoose');
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
+const testData = [
+  {
+    id: 1,
+    table: 1,
+    seat: 1,
+    time: 1.25,
+    bac: 4.0 
+  },
+  {
+    id: 2,
+    table: 1,
+    seat: 2,
+    time: 1.25,
+    bac: 2.7 
+  },
+  {
+    id: 3,
+    table: 2,
+    seat: 1,
+    time: 0.75,
+    bac: 1.0 
+  },
+  {
+    id: 4,
+    table: 2,
+    seat: 2,
+    time: 0.75,
+    bac: 0.0 
+  },
+  {
+    id: 5,
+    table: 99,
+    seat: 5,
+    time: 1.15,
+    bac: 2.1 
+  }
+];
+
+app.get('/api/patrons/', (req, res) => {
+  res.json(testData);
+});
+
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  dbConnect();
+  runServer();
 }
 
 module.exports = {app};
